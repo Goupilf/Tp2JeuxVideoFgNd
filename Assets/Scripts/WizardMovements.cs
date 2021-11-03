4b16583ae1042b661052bd3582bb5fe659eb0d00
Goupilf <45235638+Goupilf@users.noreply.github.com>
2021-11-03 12:12:30 -0400

    movementscript

--- Assets/Scripts/WizardMovements.cs
+++ Assets/Scripts/WizardMovements.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,61 @@ using UnityEngine;
 public class WizardMovements : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    [SerializeField] private new string tag;
+    private string blueWizardTag = "Blue Wizard";
+    private string greenWizardTag = "Green Wizard";
+    private string blueTowerTag = "Blue Side Tower";
+    private string greenTowerTag = "Green Side Tower";
+    private float moveSpeed = 1;
+    private float step;
+    private GameObject[] list;
     void Start()
     {
-        
+        tag = gameObject.tag;
     }
 
     // Update is called once per frame
     void Update()
     {
+        step = moveSpeed * Time.deltaTime;
+        if (tag == blueWizardTag)
+        {
+            MoveTowards(greenWizardTag);
+        }
+        else if (tag == greenWizardTag)
+        {
+            MoveTowards(blueWizardTag);
+        }
+    }
+
+    private void MoveTowards(string wizardTag)
+    {
+        string towerTag = "";
         
+        if (wizardTag == blueWizardTag)
+        {
+            towerTag = blueTowerTag;
+        }
+        else if (wizardTag == greenWizardTag)
+        {
+            towerTag = greenTowerTag;
+        }
+
+        list = GameObject.FindGameObjectsWithTag(towerTag);
+        
+        GameObject closestTower = list[1];
+        Debug.Log(closestTower);
+        float dist = 9999;
+        for (int i = 0; i < list.Length; i++)
+        {
+           float tempdist = Vector3.Distance(transform.position, list[i].transform.position);
+            if (tempdist < dist)
+            {
+                dist = tempdist;
+                closestTower = list[i]; 
+            }
+
+        }
+        transform.position = Vector3.MoveTowards(transform.position, closestTower.transform.position, step);
     }
 }
