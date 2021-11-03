e20422efca47bdf5a269314eabed8157e1de7fd2
Goupilf <45235638+Goupilf@users.noreply.github.com>
2021-11-03 10:38:53 -0400

    refactor scene

--- Assets/Scripts/WizardMovements.cs
+++ Assets/Scripts/WizardMovements.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class WizardMovements : MonoBehaviour
 {
-
     // Start is called before the first frame update
+    
     void Start()
     {
         
